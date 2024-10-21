0b4326a5a9dd2b32ba1f6ec2b530f6c267251bb8
ARKA GHOSH <arka25.cp@gmail.com>
2024-10-21 23:31:03 +0530

    Added "Hello_World" in Scheme Language

---
+++ hello_world.scm
@@ -0,0 +1,4 @@
+#lang racket  ; Specifying the language (Racket is a popular dialect of Scheme Language)
+
+(display "Hello,World!")  ; Output "Hello,World!" to  console
+(newline)                 ; Move to the next line after printing previous

625ca85743e408e3a58a50eb41376b8c51c1cda8
Sarvaksh-Shukla <98343496+Sarvaksh-Shukla@users.noreply.github.com>
2025-10-02 00:57:23 +0530

    Create hello_world(prolog).pl
    Prolog has extension conflict with perl (*.pl)

---
+++ hello_world(prolog).pl
@@ -0,0 +1,7 @@
+% Hello World in Prolog
+% Prolog or PROgramming in LOGics is a logical and declarative programming language. 
+%It is one major example of the fourth generation language that supports the declarative programming paradigm.
+main :-
+    write('Hello, World!'), nl.
+
+:- initialization(main).

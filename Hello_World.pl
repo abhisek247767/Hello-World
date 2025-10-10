c099eba7d61a6fa37f838e6ce205344cbdd84d78
Sarvaksh-Shukla <98343496+Sarvaksh-Shukla@users.noreply.github.com>
2025-10-10 18:38:17 +0530

    Update Hello_World.pl

--- Hello_World.pl
+++ Hello_World.pl
@@ -1,3 +1,7 @@
+# ProLog Language
+#short form of LOGical PROgramming. It is a logical and declarative programming language.
+#Before diving deep into the concepts of Prolog, let us first understand what exactly logical programming is.
+
 main :-
     write('Hello, World!'), nl.
 

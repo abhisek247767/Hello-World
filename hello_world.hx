04ff0fa95fc0f85a0bbd45c995d64f88246d97be
Emmanuel Simon <musavili2022@gmail.com>
2025-10-22 13:51:23 +0300

    Added hello_world in Haxe Language

---
+++ hello_world.hx
@@ -0,0 +1,9 @@
+// This is a simple Haxe program that prints "Hello, World!" to the console.
+class HelloWorld {
+    
+    // The main entry point of the program.
+    static public function main():Void {
+        // Outputs the text "Hello, World!" to the console.
+        trace("Hello, World!");
+    }
+}

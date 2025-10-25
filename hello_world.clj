d14b005867827d4caae35c54815b84a40af1d3da
Daniel Tayal <tayaldaniel2@gmail.com>
2025-10-25 21:00:22 +0530

    Update hello_world.clj

--- hello_world.clj
+++ hello_world.clj
@@ -1,8 +1,9 @@
 ; Hello World program in Clojure
 ; This is a comment
 
-(ns hello-world)
+(ns hello-world) ;creates a namespace (like a package in Java)
 
+; creating -main function
 (defn -main
   "A simple Hello World function"
   []

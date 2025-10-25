5c0f566074af8b7548b4e5339d60a360ad364f20
Daniel Tayal <tayaldaniel2@gmail.com>
2025-10-25 20:59:17 +0530

    Create hello_world.clj

---
+++ hello_world.clj
@@ -0,0 +1,12 @@
+; Hello World program in Clojure
+; This is a comment
+
+(ns hello-world)
+
+(defn -main
+  "A simple Hello World function"
+  []
+  (println "Hello, World!"))
+
+; Execute the main function
+(-main)
